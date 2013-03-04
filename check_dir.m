3a5f3b5a8bfcde46b2b33788cb693fc721a2d0d2
shanqing-cai <shanqing.cai@gmail.com>
2013-03-04 00:09:10 -0500

    Incoporated the creation option.

--- check_dir.m
+++ check_dir.m
@@ -1,5 +1,14 @@
-function check_dir(dirName)
-if ~isdir(dirName)
-    error('Failed to find directory: %s', dirName);
+function check_dir(dirName, varargin)
+if isempty(fsic(varargin, '-create'))
+    if ~isdir(dirName)
+        error('Failed to find directory: %s', dirName);
+    end
+else
+    if ~isdir(dirName)
+        mkdir(dirName);
+        if ~isdir(dirName)
+            error('Failed to create directory: %s', dirName);
+        end
+    end
 end
 return
