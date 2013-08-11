817eaec1eea1e9bc02f702bf31e4bd8b1952a83f
shanqing-cai <shanqing.cai@gmail.com>
2013-08-11 14:53:42 -0400

    Improved check_pos_int.m

--- check_pos_int.m
+++ check_pos_int.m
@@ -1,5 +1,12 @@
-function check_pos_int(x, msg)
-if x <= 0 || mod(x, 1.0) ~= 0.0
+function check_pos_int(x, msg, varargin)
+%%
+if nargin == 3
+    bAllowZero = varargin{1};
+else
+    bAllowZero = 0;
+end
+
+if (~bAllowZero && (x <= 0)) || (bAllowZero && (x < 0)) || mod(x, 1.0) ~= 0.0
     error('msg');
 end
 return
