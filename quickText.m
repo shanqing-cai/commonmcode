9eb45babde57031be5d1f8a706db234f4dca8104
shanqing-cai <shanqing.cai@gmail.com>
2014-02-26 11:14:56 -0500

    Minor changes

--- quickText.m
+++ quickText.m
@@ -1,4 +1,5 @@
 function varargout = quickText(hdl, str, fs)
-    xs = get(hdl, 'XLim');  ys = get(hdl, 'YLim');
+    xs = xlim;  
+    ys = ylim;
     text(hdl, xs(1) + 0.05 * range(xs), ys(1) + 0.05 * range(ys), str, 'FontSize', fs);
 return
